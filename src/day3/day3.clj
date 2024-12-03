92d7501633cb0ff0723c31802a751b530ced5757
Nathan Patel <baconforevery2@gmail.com>
2024-12-03 05:50:50 +0000

    day 3 refactor on->on?

--- src/day3/day3.clj
+++ src/day3/day3.clj
@@ -11,9 +11,9 @@
 (->> (slurp "inputs/day3.txt") ; slurp 😋
      (re-seq #"(mul\(\d+,\d+\)|do(n't)?\(\))") ; extract all valid instructions
      (map first) ; only take full matching group
-     (reduce (fn [[acc on] instr]
+     (reduce (fn [[acc on?] instr]
                (cond
-                 (re-matches #"mul\(\d+,\d+\)" instr) [(+ acc (apply * on (mapv Integer/parseInt (re-seq #"\d+" instr)))) on] ; extract ints, multiply
+                 (re-matches #"mul\(\d+,\d+\)" instr) [(+ acc (apply * on? (mapv Integer/parseInt (re-seq #"\d+" instr)))) on?] ; extract ints, multiply
                  (re-matches #"do\(\)" instr) [acc 1] ; if do, then on
                  :else [acc 0])) ; if don't, then off
              [0 1])
