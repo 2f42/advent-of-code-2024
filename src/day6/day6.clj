1981f92736ab41087a78c40c75f620722bd52129
Nathan Patel <baconforevery2@gmail.com>
2024-12-06 07:38:07 +0000

    day 6 refactor

--- src/day6/day6.clj
+++ src/day6/day6.clj
@@ -34,14 +34,14 @@
     :left [(dec x) y]
     :right [(inc x) y]))
 
-(defn traverse [lab width height starting-position]
-  (reduce (fn [[lab width height [x y] facing seen] _]
+(defn traverse [lab width height position]
+  (reduce (fn [[[x y] facing seen] _]
             (cond (out-of-bounds? width height (step x y facing)) (reduced [false (update seen [x y] (fnil conj #{}) facing)]) ; left the bounds
-                  (contains? (seen [x y]) facing) (reduced [true (update seen [x y] (fnil conj #{}) facing)]) ; been in this position before, exit
+                  (contains? (seen [x y]) facing) (reduced [true (update seen [x y] (fnil conj #{}) facing)]) ; been in this position before
                   :else (case (apply at lab (step x y facing))
-                          \# [lab width height [x y] (turn facing) seen] ; turn if hits an obstacle
-                          [lab width height (step x y facing) facing (update seen [x y] (fnil conj #{}) facing)]))) ; otherwise, step!
-          [lab width height starting-position :up {}]
+                          \# [[x y] (turn facing) seen] ; turn if hits an obstacle
+                          [(step x y facing) facing (update seen [x y] (fnil conj #{}) facing)]))) ; otherwise, step!
+          [position :up {}]
           (cycle '(nil))))
 
 ;; part 1
@@ -57,15 +57,11 @@
 (let [lab (read-lab)
       width (count (first lab))
       height (count lab)
-      starting-position (find-guard lab)
-      in-path (->> (traverse lab width height starting-position)
-                   (second)
-                   (keys))] ; get the set of seen positions
-  (->> (for [x (range width)
-             y (range height)
-             :when (and (some #{[x y]} in-path) ; make sure (x,y) is on the path 
-                        (not= starting-position [x y]))] ; and its not the start position
-         (update lab y #(str (subs % 0 x) "#" (subs % (inc x)))))
-       (filter #(->> (traverse % width height starting-position)
-                     (first))) ; only take paths that loop
+      starting-position (find-guard lab)]
+  (->> (traverse lab width height starting-position)
+       (second)
+       (keys) ; get the set of seen positions
+       (map (fn [[x y]]
+              (update lab y #(str (subs % 0 x) "#" (subs % (inc x)))))) ; get a new lab with the obstacle in place
+       (filter #(first (traverse % width height starting-position))) ; only take infinite cycles
        (count)))
