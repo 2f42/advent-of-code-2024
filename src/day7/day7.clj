b600c1976b84256ae13916315b025b09720a09db
Nathan Patel <baconforevery2@gmail.com>
2024-12-07 21:25:07 +0000

    day 7 fixed

--- src/day7/day7.clj
+++ src/day7/day7.clj
@@ -24,7 +24,7 @@
      (map #(list (first %) (possible-answers-2 (rest %)))) ; first number of output is target, second is all reachable answers
      (filter #(some #{(first %)} (second %))) ; filter by results that have the target in reachable answers
      (map first) ; take the targets
-           (reduce +))) ; sum
+     (reduce +)) ; sum
 
 ;; part 2 one line monstrosity
 (reduce + (map first (filter #(some #{(first %)} (second %)) (map #(list (first %) (reduce (fn [coll x] (into (into (map (partial * x) coll) (map (partial + x) coll)) (map (fn [a] (parse-long (str (str a) (str x)))) coll))) [(second %)] (drop 2 %))) (mapv #(mapv parse-long (re-seq #"\d+" %)) (clojure.string/split-lines (slurp "inputs/day7.txt")))))))
