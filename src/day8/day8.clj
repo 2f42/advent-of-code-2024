feb4e8d0d60188f26003d277a108cdfd493ac0f7
Nathan Patel <baconforevery2@gmail.com>
2024-12-09 22:03:01 +0000

    day 8 general cleanup

--- src/day8/day8.clj
+++ src/day8/day8.clj
@@ -1,31 +1,33 @@
 (ns day8.day8
   (:require
-   [clojure.string]))
+   [clojure.string :as str]))
 
 (defn read-map []
   (->> (slurp "inputs/day8.txt") ; slurp 😋
-       (clojure.string/split-lines)))
+       str/split-lines))
 
 (defn find-matches [pred s]
   (->> s
-       (reduce (fn [[m i] c]
-                 (if (pred c) [(assoc! m i c) (inc i)] ; build a map {i c} of indices i where (pred c) is true
-                     [m (inc i)]))
-               [(transient {}) 0])
-       (first)         ; only take the map
-       (persistent!))) ; make it a persistent map
+       (reduce (fn [{:keys [m i]} c]
+                 (if (pred c) {:m (assoc! m i c) :i (inc i)} ; build a map {i c} of indices i where (pred c) is true
+                     {:m m :i (inc i)}))
+               {:m (transient {})
+                :i 0})
+       :m            ; only take the map
+       persistent!)) ; make it a persistent map
 
 (defn find-antennas [coll]
   (->> coll
-       (reduce (fn [[m y] row]
-                 [(reduce (fn [m' [x c]]
-                            (update m' c (fnil conj #{}) [x y])) ; build the coords, add to the set of antennae for antenna c
-                          m (find-matches #(re-matches #"[a-zA-Z0-9]" (str %)) row)) (inc y)]) ; find all xs where the antennae are
-               [{} 0])
-       (first))) ; only take the map
+       (reduce (fn [{:keys [m y]} row]
+                 {:m (reduce (fn [m' [x c]]
+                               (update m' c (fnil conj #{}) [x y])) ; build the coords, add to the set of antennae for antenna c
+                             m (find-matches #(re-matches #"[a-zA-Z0-9]" (str %)) row))
+                  :y (inc y)}) ; find all xs where the antennae are
+               {:m {} :y 0})
+       :m)) ; only take the map
 
 (defn in-bounds? [[width height] [x y]]
-  (and (<= 0 x) (> width x) (<= 0 y) (> height y)))
+  (and (< -1 x width) (< -1 y height)))
 
 (defn get-offsets [[width height] coord offset]
   (take-while #(in-bounds? [width height] %) ; while in bounds (for part 1, change this to take 1 instead of take-while)
@@ -43,12 +45,12 @@
       width (count (first city))
       height (count city)]
   (->> city
-       (find-antennas)
+       find-antennas
        (reduce (fn [m a]
                  (->> (find-antinodes [width height] (second a))
                       (assoc! m (first a))))
                (transient {}))
-       (persistent!) ; make m persistent
+       persistent!     ; make m persistent
        (mapcat second) ; only take the values from the map
-       (distinct) ; remove duplicates
-       (count))) ; count
+       distinct        ; remove duplicates
+       count))
